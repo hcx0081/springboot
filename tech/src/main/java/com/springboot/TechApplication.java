52e5fa17d57b444cf4303216907d8232bb736398
root <623363564@qq.com>
2023-08-22 01:47:48 +0800

    修改版本

--- tech/src/main/java/com/springboot/TechApplication.java
+++ tech/src/main/java/com/springboot/TechApplication.java
@@ -1,12 +1,15 @@
 package com.springboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class TechApplication {
     public static void main(String[] args) {
         SpringApplication.run(TechApplication.class, args);
+        log.info("11{}", 1);
     }
     
 }
